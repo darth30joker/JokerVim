064e4a75f31b1ea05a986ddbe8242200bd46d169
Darth Joker <darth30joker@gmail.com>
2026-02-27 11:29:03 +0000

    Add configs for neo vim

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- basics
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+-- shortcuts
+-- set leader key to '<Space>'
+vim.mapleader = " "
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>s', ':w!<cr>')
+vim.keymap.set('n', '<leader>w', ':wq!<cr>')
+vim.keymap.set('n', '<leader>q', ':q!<cr>')
+
+vim.keymap.set('n', '<leader>t', ':NvimTreeToggle<cr>')
+
+-- lazy.nvim
+require("config.lazy")
