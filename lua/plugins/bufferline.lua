1f7cc329ad649fd59adbdf9c158a4110a8061ab9
Darth Joker <darth30joker@gmail.com>
2026-02-27 18:11:46 +0000

    Update plugins

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+    {
+      "akinsho/bufferline.nvim",
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      event = "VeryLazy",
+      opts = {
+        options = {
+          always_show_bufferline = false,
+        },
+      },
+      config = function(_, opts)
+        require("bufferline").setup(opts)
+        -- Fix bufferline when restoring a session
+        vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+          callback = function()
+            vim.schedule(function()
+              pcall(nvim_bufferline)
+            end)
+          end,
+        })
+      end,
+    }
+}
